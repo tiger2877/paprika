4fc0f046cca515f9f0438f5338abe1be5af633cc
tiger2877 <pcahyadi@gmail.com>
2019-03-07 08:25:13 -0500

    initial layout

--- server.js
+++ server.js
@@ -1,47 +1,41 @@
-require("dotenv").config();
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
-var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-var db = require("./models");
-
+// *** Sets up the Express App
+// =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// *** Sets up the Express app to handle data parsing
+// =============================================================
+// Serve static content for the app from the "public" directory
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// *** Set Handlebars
+// =============================================================
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// *** Import routes and give server access
+// =============================================================
+var routes = require("./controllers/foodController.js");
+app.use(routes);
 
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// *** Start server to listen to client requests
+// =============================================================
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-module.exports = app;
