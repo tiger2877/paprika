3d3be9ad0cde87ac7b6a2a5913d9c0aaa996faa1
tiger2877 <pcahyadi@gmail.com>
2019-03-07 07:57:35 -0500

    initial files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+DROP DATABASE IF EXISTS food_db;
+CREATE DATABASE food_db;
+USE food_db;
+
+CREATE TABLE foods
+(
+	id int NOT NULL AUTO_INCREMENT,
+	food_name varchar(255) NOT NULL,
+	checkout BOOLEAN NOT NULL DEFAULT false,
+	PRIMARY KEY (id)
+);
