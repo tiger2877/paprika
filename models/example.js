6ab22ae9e2ee4dc4c6a536793686860759c87de2
Pryambudhi Cahyadi <pcahyadi@gmail.com>
2019-03-09 12:08:58 -0500

    Update example.js

--- models/example.js
+++ models/example.js
@@ -1,7 +1,27 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
-};
+<div class="container">
+  <div class="row">
+    <div class="col-8 offset-2">
+      <div class="card mt-4">
+        <div class="card-body">
+          <h5 class="card-title">Nutrition Information:</h5>
+          <!--Display Calories, Protien, Sugar, Fat, Carbs-->
+          <p>
+            <strong>ID</strong>: {{recent.id}}</p>
+          <p>
+          <p>
+            <strong>Calories</strong>: {{recent.kCal}}</p>
+          <p>
+            <strong>Protein:</strong> {{recent.PROCNT}}</p>
+          <p>
+            <strong>Sugar</strong> {{recent.SUGAR}}</p>
+          <p>
+            <strong>Fat</strong> {{recent.FAT}}</p>
+          <p>
+            <strong>Carbs</strong> {{recent.CHOCDF}}</p>
+
+            <a href="/">← Back To Home</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
